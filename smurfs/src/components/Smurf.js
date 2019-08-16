f75b4956990bfaf5b912f5cfb3181ae50dd64cce
Andrew <andrew.mkal@outlook.com>
2019-08-16 12:37:05 -0500

    Styling on smurf cards

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -2,32 +2,60 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux'
 import { deleteSmurf, updateSmurf } from '../actions'
 import './Smurf.scss'
-import { Button, Header, Modal } from 'semantic-ui-react'
+import { Button, Header, Modal, Card, Icon, Image } from 'semantic-ui-react'
+import picture from './smurf.png'
 
 const Smurf = ({ smurf, deleteSmurf, updateSmurf }) => {
 
-    const [ currentSmurf, setCurrentSmurf ] = useState(smurf)
+    const [currentSmurf, setCurrentSmurf] = useState(smurf)
     const handleChanges = e => {
-        setCurrentSmurf({...currentSmurf, [e.target.name]: e.target.value} )
+        setCurrentSmurf({ ...currentSmurf, [e.target.name]: e.target.value })
     }
 
     return (
         <div className='smurf'>
-            <h1>{smurf.name}</h1>
-            {/* <h3>{smurf.age}</h3>
-            <h3>{smurf.height}</h3> */}
-            {/* <button onClick={_ => deleteSmurf(smurf.id)}>Delete</button> */}
-            <Modal size='mini' trigger={<Button>More Info!</Button>}>
-                <Modal.Content className='smurfModal'>
-                    <Modal.Description>
-                        <Header><input name='name' value={currentSmurf.name} onChange={handleChanges}/></Header>
-                        <p><input name='age' value={currentSmurf.age} onChange={handleChanges}/></p>
-                        <p><input name='height' value={currentSmurf.height} onChange={handleChanges}/></p>
-                    </Modal.Description>
-                    <Button onClick={_ => deleteSmurf(smurf.id)}>Delete</Button>
-                    <Button onClick={_ => updateSmurf(currentSmurf)}>Update</Button>
+            {/* <h1>{smurf.name}</h1> */}
+            {/* <Modal size='mini' trigger={<Button>More Info!</Button>}>
+                <Modal.Content>
+                    <div className='smurfModal'>
+                        <Modal.Description>
+                            <Header>NAME: <br /> <input name='name' value={currentSmurf.name} onChange={handleChanges} /></Header>
+                            <p>AGE: <br /> <input name='age' value={currentSmurf.age} onChange={handleChanges} /></p>
+                            <p>HEIGHT: <br /> <input name='height' value={currentSmurf.height} onChange={handleChanges} /></p>
+                        </Modal.Description>
+                        <div className='smurfModalButtons'>
+                            <Button className='smurfModalButton' color='red' onClick={_ => deleteSmurf(smurf.id)}>Delete</Button>
+                            <Button className='smurfModalButton' color='green' onClick={_ => updateSmurf(currentSmurf)}>Update</Button>
+                        </div>
+                    </div>
                 </Modal.Content>
-            </Modal>
+            </Modal> */}
+            <Card className='smurfCard'>
+                <Image src={picture} wrapped ui={false} />
+                <Card.Content>
+                    <Card.Header>{smurf.name}</Card.Header>
+                    <Card.Meta>
+                        <span className='date'>Age: {smurf.age}</span>
+                    </Card.Meta>
+                    <Card.Description>
+                    <Modal size='mini' trigger={<Button>More Info!</Button>}>
+                        <Modal.Content>
+                            <div className='smurfModal'>
+                                <Modal.Description>
+                                    <Header>NAME: <br /> <input name='name' value={currentSmurf.name} onChange={handleChanges} /></Header>
+                                    <p>AGE: <br /> <input name='age' value={currentSmurf.age} onChange={handleChanges} /></p>
+                                    <p>HEIGHT: <br /> <input name='height' value={currentSmurf.height} onChange={handleChanges} /></p>
+                                </Modal.Description>
+                                <div className='smurfModalButtons'>
+                                    <Button className='smurfModalButton' color='red' onClick={_ => deleteSmurf(smurf.id)}>Delete</Button>
+                                    <Button className='smurfModalButton' color='green' onClick={_ => updateSmurf(currentSmurf)}>Update</Button>
+                                </div>
+                            </div>
+                        </Modal.Content>
+                    </Modal>
+                    </Card.Description>
+                </Card.Content>
+            </Card>
         </div>
     )
 }
